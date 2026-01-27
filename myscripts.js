61e31ca19216cfb1ed55f47b6c7e138c38b6051a
bangChiem <141431115+bangChiem@users.noreply.github.com>
2026-01-27 17:06:22 -0500

    removed typewriter text and font styling for hero page

--- myscripts.js
+++ myscripts.js
@@ -1,34 +1,3 @@
-var i = 0;
-var j = 0;
-let second_header = "I am a aspiring\u0020";
-var txtArray = ['full stack engineer', 'Active Learning Researcher', 'Volleyball Player'];
-var speed = 50; // The speed/duration of the effect in milliseconds
-
-document.addEventListener('DOMContentLoaded', function () {
-    typeWriter();
-});
-
-function typeWriter() {
-    if (i < txtArray[j].length) {
-        document.getElementById("text-display").innerHTML += txtArray[j].charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    } else {
-        setTimeout(eraseWriter, 1000); // Pause before erasing
-    }
-}
-
-function eraseWriter() {
-    if (i >= 0) {
-        document.getElementById("text-display").innerHTML = second_header.concat(txtArray[j].substring(0, i));
-        i--;
-        setTimeout(eraseWriter, speed);
-    } else {
-        j = (j + 1) % txtArray.length; // Move to the next text in the array
-        setTimeout(typeWriter, 500); // Pause before typing the next string
-    }
-}
-
 // Get the navbar
 const navbar = document.getElementById("navbar");
 
